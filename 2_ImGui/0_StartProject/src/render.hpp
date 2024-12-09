a150224d8accc8d98413d86539ceb0a54c662243
Felix Vargas Jr <felixvjr0@gmail.com>
2024-12-09 18:45:19 -0500

    File Explorer :->Displaying files and directories :->Showing selected folders/files :->Allowing a filter though system :-> enable file name changes

--- 2_ImGui/0_StartProject/src/render.hpp
+++ 2_ImGui/0_StartProject/src/render.hpp
@@ -2,17 +2,33 @@
 
 #include <cstdint>
 #include <string_view>
+#include <filesystem>
+
+
+namespace fs = std::filesystem;
 
 class WindowClass
 {
 public:
+    WindowClass()
+        : currentDir(fs::current_path()),
+          select_Entry(fs::path()){}; // starting at the path of our own exe.
+
     void Draw(std::string_view label);
+
+private:
+    fs::path currentDir;
+    fs::path select_Entry;
+    
 
 private:
     void DrawMenu();
     void DrawFilter();
     void DrawActions();
     void DrawContent();
+    void OpenFileWDefaultEditor();
+    void renameFilePopup();
+    void deleteFilePopup();
 };
 
 void render(WindowClass &window_obj);
